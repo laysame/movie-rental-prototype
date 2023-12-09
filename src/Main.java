93d169e401a16dd73bf5cd59d9956bb818dfc73b
Laysa Souza <karollinylaysa@gmail.com>
2023-12-09 14:52:08 +0000

    initial commit - TODOS

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        TODO:
+            Allow users to create and log into accounts with an email and a password.
+            Allow a user to rent a movie.
+            Movies and their prices will initially be provided in a csv file.
+            When a customer is charged, output the customer, movie, and price to the console.
+            Rental duration is 1 minute for each movie.
+            Keep track of what movies a user has rented.
+            Recommend to users the 5 most rented movies of the past 5 minutes.
+         */
+        /*
+        TODO:
+            Making the code easily adaptable to, and implementing, other/variable rental durations e.g. can rent for 1 day or 1 week.
+            Making the code easily adaptable to, and implementing, data persistence, such as a database.
+         */
+        System.out.println("Hello world!");
+    }
+}
